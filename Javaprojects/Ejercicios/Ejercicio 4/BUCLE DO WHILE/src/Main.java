69cdbdba2cd171cc279c5ee7f6c975926e6efd07
Artbaddon <sebarsomo@gmail.com>
2022-11-14 22:58:08 -0500

    v

---
+++ Javaprojects/Ejercicios/Ejercicio 4/BUCLE DO WHILE/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroWhile = 2;
+        System.out.println("Bucle Do while");
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        }while (numeroWhile < 3);
+
+    }
+}
--- Javaprojects/Ejercicios/Ejercicio 4/BUCLE WHILE/src/Main.java
+++ Javaprojects/Ejercicios/Ejercicio 4/BUCLE WHILE/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
-    
+
     public static void main(String[] args) {
 
+        System.out.println("Bucle while");
+            int numeroWhile = 0;
+            while (numeroWhile < 3){
+                numeroWhile = numeroWhile + 1;
+                System.out.println(numeroWhile);
+        }
     }
+
+
+
+
+
+
 }
