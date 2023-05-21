2d3b24f7ee0d5faf46ec8d500e6a93e11eaaa68f
Artbaddon <sebarsomo@gmail.com>
2023-05-21 16:39:43 -0500

    new exercise

--- Javaprojects/practices/ejercicio tema 9 public/src/App.java
+++ Javaprojects/practices/ejercicio tema 9 public/src/App.java
@@ -2,14 +2,28 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Persona persona = new Persona();
-        persona.nombre("Jaime");
+        persona.nombre("Jaime Buckingham");
         persona.edad(14);
         persona.telefono("13145");
 
-        System.out.println("La persona se llama " + persona.nombre + "\n tiene " +persona.edad +" y.o \n "+ "su telefono es: " + persona.telefono+"\n");
-       
+        System.out.println("La persona se llama " + persona.nombre + "\ntiene " + persona.edad + " y.o \n"
+                + "su telefono es: " + persona.telefono + "\n");
 
+        Cliente cliente = new Cliente();
+        cliente.nombre("Maria Sedai");
+        cliente.edad(50);
+        cliente.telefono("6666666");
+        cliente.credito(4141);
+        System.out.println("La persona se llama " + cliente.nombre + "\ntiene " + cliente.edad + " y.o \n"
+        + "su telefono es: " + cliente.telefono + "\n" + "Su credito es : " + cliente.credito + "\n");
 
+        Trabajador trabajador = new Trabajador();
+        trabajador.nombre("Rand Al Thor");
+        trabajador.edad(34);
+        trabajador.telefono("6666666");
+        trabajador.salario(4141);
+        System.out.println("El persona se llama " + trabajador.nombre + "\ntiene " + trabajador.edad + " y.o\n"
+        + "su telefono es: " + trabajador.telefono + "\n" + "Su credito es : " + trabajador.salario + "\n");
 
     }
 }
